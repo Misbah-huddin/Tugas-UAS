5b712a33b682293d5c788e7d5e8ead88dd81c2c1
Misbah-huddin <97730821+Misbah-huddin@users.noreply.github.com>
2022-01-14 16:36:30 +0700

    Tugas Uas

---
+++ MIIS.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	char nama[25];
+	int nim;
+	char prodi[50];
+	
+	printf("\nMUHAMMAD MISBAHUDDIN IJLAL",nama);
+	printf("\nProdi Teknik Informatika",prodi);
+	printf("\n07.1.2021.0011",nim);
+	
+	return 0;
+}
